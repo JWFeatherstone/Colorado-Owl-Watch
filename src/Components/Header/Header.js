b880c3d692228636535913786474a7282df1218b
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-06-06 22:40:42 -0600

    Configure carousel, rework images and color scheme, add Header component, adjust layout, tweak map.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,13 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
-      <h1>OWL WATCH</h1>
+      <nav>
+        <img className="menu-icon" src={require('../../Images/menu-icon.svg').default} alt="menu icon"/>
+        <div className="nav-right">
+          <h1 className="site-title">COLORADO OWL WATCH</h1>
+          <img className="site-logo" src={require('../../Images/owl-icon-white.svg').default} alt="owl icon" />
+        </div>
+      </nav>
     </header>
   )
 }
